7e8f926ef0b03d16062429f4552b02339797b18a
unknown <a.adhikari6960@gmail.com>
2020-10-31 22:20:25 +0545

    code to ask user for input and initialised a1 register for 1000 characters. And loading reply's address to a0.

--- project2.s
+++ project2.s
@@ -1,4 +1,11 @@
 .data
 	invalid: .asciiz "Invalid Input"#String to print if the values are invalid
 	reply: .space 1000		#reserving the 1000 bytes of memory for userInput
-	four: .space 4			#new string with space for just four after the first non-space non-null character
+	four: .space 4			#new string with space for just four after the first non-space non-null character add 
+.text
+main:
+							#asking the user for input
+	li $v0, 8
+	la $a0, reply
+	li $a1, 1000	
+	syscall 
