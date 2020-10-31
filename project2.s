fb04aa5d59152260098f055e9a5a996b95e142f8
unknown <a.adhikari6960@gmail.com>
2020-10-31 22:50:51 +0545

    Reply's address has been initialised to a1 and and t1 has been initialised to 0 to use as a boolean later in the program. The first label, First has been started and will now work as a loop to check for the spaces are store data in the Four string.

--- project2.s
+++ project2.s
@@ -8,4 +8,10 @@ main:
 	li $v0, 8
 	la $a0, reply
 	li $a1, 1000	
-	syscall 
+	syscall
+	la $a1, reply				#loading the adress of a1 in reply to increment by 1 while checking all the values
+	li $t9, 0				#if it is 0, then a valid character is not found.
+
+First:
+	lb $a0,($a1)        			# read the character
+	addi $a1, $a1, 1			# after reading increase the pointer by 1 
