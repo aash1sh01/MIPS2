acb272946420f25e2e6eec4304f5151f6d4d0a9a
unknown <a.adhikari6960@gmail.com>
2020-10-31 22:10:25 +0545

    The data section has been initialised and space for 1000 characters has been alloted for each user input, The label invalid contains Invalid Input string to print. A new string four has been also initialised to make the working simple after 4 characters have been retrieved.

---
+++ project2.s
@@ -0,0 +1,4 @@
+.data
+	invalid: .asciiz "Invalid Input"#String to print if the values are invalid
+	reply: .space 1000		#reserving the 1000 bytes of memory for userInput
+	four: .space 4			#new string with space for just four after the first non-space non-null character
