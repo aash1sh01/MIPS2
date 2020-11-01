49505bf02b49e3b91624cfcfda5efb7e3dff00c4
unknown <a.adhikari6960@gmail.com>
2020-11-01 23:52:51 +0545

    Checking if the input is empty, has null characters or has spaces to make sure that the input will be valid as we will pass through the next steps

--- project2.s
+++ project2.s
@@ -15,3 +15,11 @@ main:
 First:
 	lb $a0,($a1)        			# read the character
 	addi $a1, $a1, 1			# after reading increase the pointer by 1 
+
+	beq $a0, 0, spaceOrEmpty		#if it is a null character check if the input is empty
+	beq $a0, 10, spaceOrEmpty		#if there is an end line character then check if the input is empty or parse through the loop to check if it has valid input
+	
+	beq $a0, 32, First			#if there is a space in front or back of the input, we just carry on with the loop until we find a non space or non endline character.
+	
+	beq $t9, 1, invalidInputError		# this code is executed after the first four characters after removing the spaces have been counted, so after that all other input is invalid.
+	
