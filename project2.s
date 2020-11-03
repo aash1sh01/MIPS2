7a89ea37307b3b5b9953566adaffd195c5532d22
unknown <a.adhikari6960@gmail.com>
2020-11-03 23:59:39 +0545

    Adding to t9, t0 and -87 to make sure 97=a would give the value 10. And after this, checking t3 and 1 and sending it to convert into base 35I

--- project2.s
+++ project2.s
@@ -70,6 +70,8 @@ Loop:
 	li $t3, 96
 	slt $t3, $t3, $t0
 	and $t3, $t3, $t2  			# if t3 and t2 are same, t3  = 1, this checks if it falls under <=96
+	addi $t9, $t0, -87
+	bne $t3, 1, invalidInputError		#when t3 is not equal to one it is invalid because less than 96 is not a valid
 
 	
 	
